bdf0fb2b3df107ad1848ba10d7c16dea353c848e
Shee Ong <sj3.ong@gmail.com>
2017-02-06 15:25:49 +0000

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = (props) => {
   return (
-      <h3>About Component</h3>
+    <div columns medium-6 large-4 small-centered>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React..</p>
+      <p>
+        Here are some of the tools used:
+        <ul>
+          <li><a href="https://facebook.github.io/react">React</a> - Javascript framework used.</li>
+        </ul>
+        <ul>
+          <li><a href="http://oepnweathermap.org">Open Weather Map</a> - Open Weather Map API is used to search for the weather data by city name.</li>
+        </ul>
+      </p>
+    </div>
   );
 }
 
