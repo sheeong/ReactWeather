94223a3e4a1caa484ead8d6414b331bdb96234f8
Shee Ong <sj3.ong@gmail.com>
2017-02-06 10:33:03 +0000

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,28 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=896627903aca1e280d2165ad7fcbf809&units=imperial';
+
+
+module.exports = {
+  getTemp: function(location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    return axios.get(requestUrl).then(function(res) {
+      //debugger;
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        console.log(res.data.name);
+        //return res.data.main.temp;
+        return {
+          location: res.data.name,
+          temp: res.data.main.temp
+        }
+      }
+    }, function(res) {
+      console.log(res);
+      //throw new Error(res.data.message);
+      throw new Error(res);
+    });
+  }
+}
